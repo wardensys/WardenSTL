7b57dd28ed573f1301a01c78145983554af70c49
WardenHD <tomas.bezruchko@gmail.com>
2026-07-22 00:39:44 +0300

    Trying to localize issue with count args...

--- tests/syntax_checks/Algorithm.t.cpp
+++ tests/syntax_checks/Algorithm.t.cpp
@@ -1,1 +1,6 @@
 #include <wstl/Algorithm.hpp>
+
+static_assert(WSTL_COUNT_ARGS(A) == 1, "");
+static_assert(WSTL_COUNT_ARGS(A, B) == 2, "");
+static_assert(WSTL_COUNT_ARGS(A, B, C) == 3, "");
+static_assert(WSTL_COUNT_ARGS(A, B, C, D, E) == 5, "");
