3b71d323e801b27c5b6b795d5820b1c051131dde
WardenHD <tomas.bezruchko@gmail.com>
2026-09-25 16:06:14 +0300

    Rewritten static assert for c++98

--- include/wstl/StaticAssert.hpp
+++ include/wstl/StaticAssert.hpp
@@ -1,5 +1,5 @@
 // Part of WardenSTL - https://github.com/WardenHD/WardenSTL
-// Copyright (c) 2025 Artem Bezruchko (WardenHD)
+// Copyright (c) 2026 Artem Bezruchko (WardenHD)
 //
 // This file is based on the Embedded Template Library (ETL)'s static assert utilities,
 // with modifications made for improved support under C++98.
@@ -14,20 +14,52 @@
 
 
 #ifdef __WSTL_CXX11__
+/// @brief A macro to trigger a static assertion based on a boolean condition
+/// @param condition The boolean condition to evaluate
+/// @param ... Optional message to display on failure (ignored in C++98)
+/// @note Do not put `false` as the condition, use `WSTL_STATIC_ASSERT_FALSE` instead for 
+/// template contexts because of C++98 limitations
+/// @ingroup utilities
 #define WSTL_STATIC_ASSERT static_assert
+
+/// @brief A macro to trigger a static assertion failure based on a template parameter
+/// @param templateParam The template parameter to evaluate (should be a type, ignored in C++11 and higher)
+/// @param ... Optional message to display on failure (ignored in C++98)
+/// @ingroup utilities
+#define WSTL_STATIC_ASSERT_FALSE(templateParam, ...) static_assert(false, __VA_ARGS__)
 #else
 namespace wstl {
     namespace __private {
         template<bool Condition>
-        class __StaticAssert;
+        struct __StaticAssert;
 
         template<>
-        class __StaticAssert<true> {};    
+        struct __StaticAssert<true> {};
+
+        template<typename T>
+        struct __StaticAssertFalse {
+            enum { Value = false };
+        };
     }
 }
 
-#define WSTL_STATIC_ASSERT(condition, ...) \
-    enum { __StaticAssertion = sizeof(__private::__StaticAssert<static_cast<bool>( (condition) )>) }
-#endif
+#define __WSTL_STATIC_ASSERT_IMPL__(condition, line) \
+    enum { WSTL_CONCATENATE(__StaticAssertion, line) = sizeof(wstl::__private::__StaticAssert<static_cast<bool>((condition))>) }
 
+/// @brief A macro to trigger a static assertion based on a boolean condition
+/// @param condition The boolean condition to evaluate
+/// @param ... Optional message to display on failure (ignored in C++98)
+/// @note Do not put `false` as the condition, use `WSTL_STATIC_ASSERT_FALSE` instead for 
+/// template contexts because of C++98 limitations
+/// @ingroup utilities
+#define WSTL_STATIC_ASSERT(condition, ...) __WSTL_STATIC_ASSERT_IMPL__(condition, __LINE__)
+
+/// @brief A macro to trigger a static assertion failure based on a template parameter
+/// @param templateParam The template parameter to evaluate (should be a type, ignored in C++11 and higher)
+/// @param ... Optional message to display on failure (ignored in C++98)
+/// @ingroup utilities
+#define WSTL_STATIC_ASSERT_FALSE(templateParam, ...) \
+    __WSTL_STATIC_ASSERT_IMPL__(wstl::__private::__StaticAssertFalse<templateParam>::Value, __LINE__)
+
+#endif
 #endif
